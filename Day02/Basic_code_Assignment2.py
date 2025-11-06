a86ea38f685c93e3baf001b58d241cdd883ad6d6
Aileen Cohen <123306307+AileenCohen@users.noreply.github.com>
2025-11-06 17:12:44 +0200

    Update Basic_code_Assignment2.py

--- Day02/Basic_code_Assignment2.py
+++ Day02/Basic_code_Assignment2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def calculate_pool_concentrations_from_qubit_data(conc_file, pool_dict):
+def calculate_pool_concentrations_from_qubit_data(conc_file: pd.DataFrame, pool_dict: dict) -> pd.DataFrame:
     samples = conc_file["Sample Name"].astype(str).tolist()
     concentrations = pd.to_numeric(conc_file["Original Sample Conc."], errors='coerce')
     
@@ -24,14 +24,14 @@ def calculate_pool_concentrations_from_qubit_data(conc_file, pool_dict):
             amount_to_take[samples[i]] = pools[-1] / conc
 
     plate = pd.DataFrame(list(amount_to_take.items()), columns=['Sample', 'Amount_to_Take'])
-
     return plate
 
 
 
-qubit_file = pd.read_csv(r'Qubit_data_example.csv')
-pool_dictionary = {'pool_1': 5, 'pool_2': 30, 'pool_3': 100}
-result = calculate_pool_concentrations_from_qubit_data(qubit_file, pool_dictionary)
 
-print(result)
+# qubit_file = pd.read_csv(r'Qubit_data_example.csv')
+# pool_dictionary = {'pool_1': 5, 'pool_2': 30, 'pool_3': 100}
+# result = calculate_pool_concentrations_from_qubit_data(qubit_file, pool_dictionary)
+
+# print(result)
 
